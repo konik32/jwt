0fb5cdd3de7d048640b86b658159c12b690f5ce8
konik32 <konik32@gmail.com>
2015-04-30 19:36:45 +0200

    Added authentication exception handling

--- jwt/src/main/java/security/jwt/JwtAuthenticationFilter.java
+++ jwt/src/main/java/security/jwt/JwtAuthenticationFilter.java
@@ -9,14 +9,15 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.util.Assert;
 import org.springframework.web.filter.GenericFilterBean;
 
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
 	private final TokenAuthenticationService tokenAuthenticationService;
+	private JwtAuthenticationExceptionHandler exceptionHadler;
 
 	public JwtAuthenticationFilter(TokenAuthenticationService taService) {
 		Assert.notNull(taService);
@@ -24,12 +25,21 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 	}
 
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse res,
-			FilterChain chain) throws IOException, ServletException {
-			Authentication auth = tokenAuthenticationService
-					.getAuthentication((HttpServletRequest) req);
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
+			ServletException {
+		try {
+			Authentication auth = tokenAuthenticationService.getAuthentication((HttpServletRequest) req);
 			if (auth != null)
 				SecurityContextHolder.getContext().setAuthentication(auth);
-		chain.doFilter(req, res);
+			chain.doFilter(req, res);
+		} catch (AuthenticationException ex) {
+			if (exceptionHadler != null)
+				exceptionHadler.onAuthenticationException(req, res, ex);
+		}
+
+	}
+
+	public void setExceptionHadler(JwtAuthenticationExceptionHandler exceptionHadler) {
+		this.exceptionHadler = exceptionHadler;
 	}
 }
